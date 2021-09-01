59291330ae07703c8f00d32b5ab5266d14f7a047
Sharath Kumar <svkumar2@ncsu.edu>
2021-09-01 19:12:55 -0400

    modified test code to be compatible with pytest

--- test/test_code.py
+++ test/test_code.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import sys
-import unittest
 
 path = str(Path(Path(__file__).parent.absolute()).parent.absolute())
 sys.path.insert(0, path)
@@ -8,11 +7,9 @@ sys.path.insert(0, path)
 import code
 
 
-class TestSum(unittest.TestCase):
-
-    def test_call_api(self):
-        self.assertEqual(code.call_api(), 200, "Should be 200")
+def test_call_api():
+    assert code.call_api() == 200
 
 
 if __name__ == '__main__':
-    unittest.main()
+    test_call_api()
